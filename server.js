610945851150d1d6fdba1038f6c0940b35cdd8c7
Furkan Sanli <furkan.sanli@INSTRMB1024.local>
2025-04-17 10:06:25 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('API is working!!!!');
+});
+
+// Sample POST endpoint
+app.post('/data', (req, res) => {
+  const receivedData = req.body;
+  res.json({ message: 'Data received!', data: receivedData });
+});
+
+app.listen(port, () => {
+  console.log(`API service running at http://localhost:${port}`);
+});
